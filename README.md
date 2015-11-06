eab60d0656d8e69388056ebce54e4358482471c7
Mats Byrkjeland <matsbyr@gmail.com>
2015-11-05 22:15:46 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Instrulator
+
+Installer script for fresh or not-so-fresh Ubuntu installs.
+Use it to install all (or at least some) of your favorite programs automatically while leaning
+back in your comfortable chair, with an ice cold beer in your hand.
+
+## How-to
+
+Download the script:
+`wget https://github.com/draperunner/intrulator/instrulator.sh`
+Make it executable:  
+`chmod +x instrulator.sh`
+Run it with sudo  
+`sudo ./instrulator.sh`
+(Optional) Delete it after you're done  
+`rm instrulator.sh`
---
+++ instrulator.sh
@@ -0,0 +1,49 @@
+# Ask users for what to install
+echo -n "Install Google Chrome? [y/n] " && read CHROME
+echo -n "Install Spotify? [y/n] " && read SPOTIFY
+echo -n "Install Node.js? [y/n] " && read NODE
+echo -n "Install gimp? [y/n] " && read GIMP
+echo -n "Install Steam? [y/n] " && read STEAM
+echo -n "Install Dropbox? [y/n] " && read DROPBOX
+echo -n "Install Virtualbox? [y/n] " && read VIRTUALBOX
+
+echo "Adding repositories and keys"
+if [ "$SPOTIFY" == y ] ; then
+  apt-add-repository -y "deb http://repository.spotify.com stable non-free"
+  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D2C19886
+fi
+
+if [ "$CHROME" == y ] ; then
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+  sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+fi
+
+echo "Updating repository list..."
+apt-get update -qq
+
+# Install Node.js using NVM
+if [ "$NODE" == y ] ; then
+  echo "Installing Node..."
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+  source ~/.profile
+  nvm install 4.2.1
+  npm install -g express
+  npm install -g nodemon
+  npm install -g bower
+fi
+
+# Install Dropbox
+if [ "$DROPBOX" == y] ; then
+  echo "Installing Dropbox..."
+  wget "https://www.dropbox.com/download?plat=lnx.x86_64"
+  mv dropbox-lnx.x86_64* ~
+  tar xzf /home/$USER/dropbox-lnx.x86_64*
+  /home/$USER/.dropbox-dist/dropboxd
+  echo -e "[Desktop Entry]\nType=Application\nExec=/home/$USER/.dropbox-dist/dropboxd\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[en_US]=Dropbox\nName=Dropbox\nComment[en_US]=Start Dropbox\nComment=Start Dropbox\n" >> /home/$USER/.config/autostart/dropboxd.desktop
+fi
+
+if [ "$CHROME" == y ] ; then echo "Installing Chrome..." && apt-get install -qq google-chrome-stable ; fi
+if [ "$SPOTIFY" == y ] ; then echo "Installing Spotify..." && apt-get install -qq spotify-client ; fi
+if [ "$GIMP" == y ] ; then echo "Installing gimp..." && apt-get install -qq gimp ; fi
+if [ "$STEAM" == y ] ; then echo "Installing Steam..." && apt-get install -qq steam ; fi
+if [ "$VIRTUALBOX" == y ] ; then echo "Installing Virtualbox..." && apt-get install -qq virtualbox-qt
